d514476b4c56c73c6e7e31eefdcdd341f3f2dd8f
Mathias Verraes <mathias@verraes.net>
2014-06-05 17:12:58 +0200

    cleanup

--- src/EventCentric/Protection/AggregateRoot/AggregateRootEntity.php
+++ src/EventCentric/Protection/AggregateRoot/AggregateRootEntity.php
@@ -2,13 +2,12 @@
 
 namespace EventCentric\Protection\AggregateRoot;
 
-use EventCentric\Protection\AggregateRoot\AggregateRoot;
 use EventCentric\Protection\AggregateRoot\EventSourcing;
 use EventCentric\Protection\AggregateRoot\Reconstitution;
 use EventCentric\When\ConventionBased\ConventionBasedWhen;
 
 abstract class AggregateRootEntity implements AggregateRoot
 {
-    use \EventCentric\Protection\AggregateRoot\EventSourcing, \EventCentric\Protection\AggregateRoot\Reconstitution, ConventionBasedWhen;
-    final private function __construct(){}
+    use EventSourcing, Reconstitution, ConventionBasedWhen;
+    final protected function __construct(){}
 } 
