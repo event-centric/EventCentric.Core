edc10b0f1d06fb376c80c1809d8295a0732eadef
Mathias Verraes <mathias@verraes.net>
2014-06-19 16:55:05 +0200

    contract vo

---
+++ src/EventCentric/Contracts/Contract.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EventCentric\Contracts;
+
+use Assert;
+
+final class Contract
+{
+    /**
+     * @var string
+     */
+    private $contract;
+
+    private function __construct($contract)
+    {
+        $this->contract = $contract;
+    }
+
+    public static function with($name)
+    {
+        Assert\that($name)->string()->betweenLength(1, 255);
+        return new static($name);
+    }
+
+    public function __toString()
+    {
+        return $this->contract;
+    }
+
+    public function equals(Contract $other)
+    {
+        return $this->contract == $other->contract;
+    }
+
+} 
