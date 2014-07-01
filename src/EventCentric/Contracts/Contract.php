990bb5abe802710b4fb25b20c44ac9779c1f75b3
Mathias Verraes <mathias@verraes.net>
2014-07-01 21:27:05 +0200

    Contract::canonicalFrom($object)

--- src/EventCentric/Contracts/Contract.php
+++ src/EventCentric/Contracts/Contract.php
@@ -3,6 +3,7 @@
 namespace EventCentric\Contracts;
 
 use Assert;
+use Verraes\ClassFunctions\ClassFunctions;
 
 final class Contract
 {
@@ -24,6 +25,16 @@ final class Contract
     {
         Assert\that($name)->string()->betweenLength(1, 255);
         return new static($name);
+    }
+
+    /**
+     * Make a contract from an object's namespace using dots instead of backslashes
+     * @param $object
+     * @return Contract
+     */
+    public static function canonicalFrom($object)
+    {
+        return new Contract(ClassFunctions::canonical($object));
     }
 
     public function __toString()
