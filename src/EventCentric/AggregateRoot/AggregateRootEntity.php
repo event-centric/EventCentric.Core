9a6a02b17f52a7ebbc0557ca824ec1e5dc23a698
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-08-20 17:08:50 +0200

    ConventionalWhen is already imported as a used class

--- src/EventCentric/AggregateRoot/AggregateRootEntity.php
+++ src/EventCentric/AggregateRoot/AggregateRootEntity.php
@@ -11,6 +11,7 @@ use EventCentric\When\ConventionalWhen;
  */
 abstract class AggregateRootEntity implements AggregateRoot
 {
-    use EventSourcing, Reconstitution, \EventCentric\When\ConventionalWhen;
+    use EventSourcing, Reconstitution, ConventionalWhen;
+
     protected function __construct() {}
-} 
+}
